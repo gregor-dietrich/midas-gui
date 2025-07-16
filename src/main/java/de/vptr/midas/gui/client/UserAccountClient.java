ec4a700b2febe4482a399158669e2c00c79b21d1
Gregor Dietrich <shogun-empfohlen-98@icloud.com>
2025-07-16 11:27:36 +0200

    Refactor UserAccountClient and UserAccount to streamline API methods and remove unused properties

--- src/main/java/de/vptr/midas/gui/client/UserAccountClient.java
+++ src/main/java/de/vptr/midas/gui/client/UserAccountClient.java
@@ -15,48 +15,48 @@ import jakarta.ws.rs.core.Response;
 @Consumes(MediaType.APPLICATION_JSON)
 public interface UserAccountClient {
 
-    @GET
-    List<UserAccount> getAllAccounts(@HeaderParam("Authorization") String authorization);
+        @GET
+        List<UserAccount> getAllAccounts(@HeaderParam("Authorization") String authorization);
 
-    @GET
-    @Path("/{id}")
-    Response getAccount(@PathParam("id") Long id, @HeaderParam("Authorization") String authorization);
+        @GET
+        @Path("/{id}")
+        Response getAccount(@PathParam("id") Long id, @HeaderParam("Authorization") String authorization);
 
-    @GET
-    @Path("/name/{name}")
-    Response getAccountByName(@PathParam("name") String name, @HeaderParam("Authorization") String authorization);
+        @GET
+        @Path("/name/{name}")
+        Response getAccountByName(@PathParam("name") String name, @HeaderParam("Authorization") String authorization);
 
-    @GET
-    @Path("/user/{userId}")
-    List<UserAccount> getAccountsByUser(@PathParam("userId") Long userId,
-            @HeaderParam("Authorization") String authorization);
+        @GET
+        @Path("/user/{userId}")
+        List<UserAccount> getAccountsByUser(@PathParam("userId") Long userId,
+                        @HeaderParam("Authorization") String authorization);
 
-    @GET
-    @Path("/{id}/outgoing-payments")
-    Response getOutgoingPayments(@PathParam("id") Long id, @HeaderParam("Authorization") String authorization);
+        @GET
+        @Path("/{id}/payments/outgoing")
+        Response getOutgoingPayments(@PathParam("id") Long id, @HeaderParam("Authorization") String authorization);
 
-    @GET
-    @Path("/{id}/incoming-payments")
-    Response getIncomingPayments(@PathParam("id") Long id, @HeaderParam("Authorization") String authorization);
+        @GET
+        @Path("/{id}/payments/incoming")
+        Response getIncomingPayments(@PathParam("id") Long id, @HeaderParam("Authorization") String authorization);
 
-    @GET
-    @Path("/{id}/associated-users")
-    Response getAssociatedUsers(@PathParam("id") Long id, @HeaderParam("Authorization") String authorization);
+        @GET
+        @Path("/{id}/users")
+        Response getAssociatedUsers(@PathParam("id") Long id, @HeaderParam("Authorization") String authorization);
 
-    @GET
-    @Path("/search")
-    List<UserAccount> searchAccounts(@QueryParam("query") String query,
-            @HeaderParam("Authorization") String authorization);
+        @GET
+        @Path("/search")
+        List<UserAccount> searchAccounts(@QueryParam("query") String query,
+                        @HeaderParam("Authorization") String authorization);
 
-    @POST
-    Response createAccount(UserAccount account, @HeaderParam("Authorization") String authorization);
+        @POST
+        Response createAccount(UserAccount account, @HeaderParam("Authorization") String authorization);
 
-    @PUT
-    @Path("/{id}")
-    Response updateAccount(@PathParam("id") Long id, UserAccount account,
-            @HeaderParam("Authorization") String authorization);
+        @PUT
+        @Path("/{id}")
+        Response updateAccount(@PathParam("id") Long id, UserAccount account,
+                        @HeaderParam("Authorization") String authorization);
 
-    @DELETE
-    @Path("/{id}")
-    Response deleteAccount(@PathParam("id") Long id, @HeaderParam("Authorization") String authorization);
+        @DELETE
+        @Path("/{id}")
+        Response deleteAccount(@PathParam("id") Long id, @HeaderParam("Authorization") String authorization);
 }
--- src/main/java/de/vptr/midas/gui/dto/UserAccount.java
+++ src/main/java/de/vptr/midas/gui/dto/UserAccount.java
@@ -1,7 +1,5 @@
 package de.vptr.midas.gui.dto;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class UserAccount {
@@ -11,15 +9,6 @@ public class UserAccount {
 
     @JsonProperty("name")
     public String name;
-
-    @JsonProperty("userAccountMetas")
-    public List<UserAccountMeta> userAccountMetas;
-
-    @JsonProperty("outgoingPayments")
-    public List<UserPayment> outgoingPayments;
-
-    @JsonProperty("incomingPayments")
-    public List<UserPayment> incomingPayments;
 
     public UserAccount() {
         // Default constructor for Jackson
@@ -37,18 +26,6 @@ public class UserAccount {
         return this.name;
     }
 
-    public List<UserAccountMeta> getUserAccountMetas() {
-        return this.userAccountMetas;
-    }
-
-    public List<UserPayment> getOutgoingPayments() {
-        return this.outgoingPayments;
-    }
-
-    public List<UserPayment> getIncomingPayments() {
-        return this.incomingPayments;
-    }
-
     public void setId(final Long id) {
         this.id = id;
     }
@@ -57,26 +34,11 @@ public class UserAccount {
         this.name = name;
     }
 
-    public void setUserAccountMetas(final List<UserAccountMeta> userAccountMetas) {
-        this.userAccountMetas = userAccountMetas;
-    }
-
-    public void setOutgoingPayments(final List<UserPayment> outgoingPayments) {
-        this.outgoingPayments = outgoingPayments;
-    }
-
-    public void setIncomingPayments(final List<UserPayment> incomingPayments) {
-        this.incomingPayments = incomingPayments;
-    }
-
     @Override
     public String toString() {
         return "UserAccount{" +
                 "id=" + this.id +
                 ", name='" + this.name + '\'' +
-                ", userAccountMetas=" + (this.userAccountMetas != null ? this.userAccountMetas.size() : 0) + " items" +
-                ", outgoingPayments=" + (this.outgoingPayments != null ? this.outgoingPayments.size() : 0) + " items" +
-                ", incomingPayments=" + (this.incomingPayments != null ? this.incomingPayments.size() : 0) + " items" +
                 '}';
     }
 }
